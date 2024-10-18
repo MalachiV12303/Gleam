45f82e26a4192dbfa5aa4bac618ec10424b70a40
malac <malachivalle12303@gmail.com>
2024-10-18 17:50:44 -0400

    added seed and example data

---
+++ app/lib/data.ts
@@ -0,0 +1,20 @@
+const cameras = [
+    {
+        id: '4447777',
+        name: 'EOS Rebel T7',
+        type: 'DSLR',
+        brand: 'Canon',
+        megapixels: 24.1,
+        price: 599.99,
+    },
+    {
+        id: '0909090',
+        name: 'Z30',
+        type: 'Mirrorless',
+        brand: 'Nikon',
+        megapixels: 20.9,
+        price: 849.99,
+    },
+  ];
+export { cameras };
+  
