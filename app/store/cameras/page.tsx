73da58f0d35983e8f580c03a85e35799d9147b71
malac <malachivalle12303@gmail.com>
2024-10-22 18:57:33 -0400

    store page update

--- app/store/cameras/page.tsx
+++ app/store/cameras/page.tsx
@@ -17,15 +17,14 @@ export default async function Page({
             ? [searchParams.filter]
             : [];
     let cameras = await fetchFilteredCameras(filters);
-    console.log(cameras);
     return (
         <>
             <div className="flex-col mx-auto w-9/12 mt-[4rem] max-h-screen content-center">
-                <div className="flex h-[65vh]">
-                    <div className="basis-1/2">
+                <div className="flex h-[65vh] gap-8 ">
+                    <div className="basis-1/4 ">
                         <FiltersPanel filters={filters} />
                     </div>
-                    <div className="basis-1/2">
+                    <div className="basis-3/4">
                         <Suspense fallback={<div>loading...</div>}>
                             <ItemPanel query={cameras} />
                         </Suspense>
