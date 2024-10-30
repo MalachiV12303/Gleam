722b38533fea0628a8f1d3e7094f9eee01c433e8
malac <malachivalle12303@gmail.com>
2024-10-30 17:48:31 -0400

    lint

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -3,11 +3,12 @@ import { raleway } from "@/app/ui/fonts"
 import React from 'react';
 
 export default function Page({ params }: { params: { id: string } }) {
+
     return (
         <>
             <div className={`${raleway.className} flex-col mx-auto w-full sm:w-9/12 mt-12 sm:mt-16 max-h-screen`}>
                 <div className="p-4">
-                    Item Screen
+                    Item Screen {params.id}
                 </div>
             </div>
         </>
