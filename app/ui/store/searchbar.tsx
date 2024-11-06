fe7c8a12c13a32a7e8e441a599817b4631d99213
MequelB <mjbarborak@gmail.com>
2024-11-06 16:43:46 -0500

    debouncing

--- app/ui/store/searchbar.tsx
+++ app/ui/store/searchbar.tsx
@@ -3,16 +3,22 @@
 import { searchParams } from '@/app/lib/searchParams'
 import { useQueryState } from 'nuqs'
 import React from 'react'
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function SearchBar() {
     const [search, setSearch] = useQueryState('search', searchParams.search.withOptions({
         shallow: false
     }))
 
+    const updateSearch = useDebouncedCallback((term) => {
+        console.log("searching for: " + term )
+        setSearch(term);
+    }, 500); 
+    
     return (
         <div className="flex w-full">
             <label>
-                <input id="searchbar" value={search || ''} onChange={e => setSearch(e.target.value)}
+                <input id="searchbar" defaultValue={search} onChange={e => updateSearch(e.target.value)}
                     placeholder="search..."
                     className="opacity-80 w-full border-transparent outline-none bg-transparent" />
             </label>
