29b411709584a59ccc891b31e7be8b4b135711d8
MalachiV12303 <malachivalle12303@gmail.com>
2024-11-08 15:21:12 -0500

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
@@ -31,6 +32,6 @@ const config: Config = {
     }
     
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
