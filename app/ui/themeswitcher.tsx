2d398a668b82efd8865204a71f7ac7536183e7a9
MalachiV12303 <malachivalle12303@gmail.com>
2024-11-09 21:25:43 -0500

    theme switcher update

--- app/ui/themeswitcher.tsx
+++ app/ui/themeswitcher.tsx
@@ -1,29 +1,38 @@
-"use client";
+"use client"
 
-//import { Switch } from "@nextui-org/react";
-import clsx from "clsx";
-import {useTheme} from "next-themes";
-import { useEffect, useState } from "react";
+import { Switch } from "@nextui-org/react"
+import {useTheme} from "next-themes"
+import React from "react"
+import { useEffect, useState } from "react"
 
 export function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const { setTheme } = useTheme()
+  const [isSelected, setIsSelected] = React.useState(false)
+
   useEffect(() => {
     setMounted(true)
   }, [])
   if(!mounted) return null
   return (
     <div className="flex gap-4">
-        {/* <Switch
+        <Switch
         defaultSelected
+        isSelected={isSelected}
+        classNames={{
+          wrapper: "bg-primary",
+          thumb: "bg-background",
+          endContent: "text-background mr-[5px]",
+        }}
+        onValueChange={(e) => {
+          setIsSelected(e)
+          isSelected ? setTheme("darker") : setTheme("lighter")
+        }}
         size="lg"
-        color="primary"
-        startContent={<span>☼</span>}
-        endContent={<span>☾</span>}
+        startContent={<span>☾</span>}
+        endContent={<span>☼</span>}
       >
-      </Switch> */}
-      <div className={clsx("flex rounded-full	justify-center w-6 h-6 bg-background", { "bg-foreground text-background": theme === 'darker' })}><button onClick={() => setTheme('lighter')}><span>☼</span></button></div>
-      <div className={clsx("flex rounded-full	justify-center w-6 h-6 bg-background", { "bg-foreground text-background": theme === 'lighter' })}><button onClick={() => setTheme('darker')}><span>☾</span></button></div>
+      </Switch>
     </div>
   )
 };
