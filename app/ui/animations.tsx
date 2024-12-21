8bff73fb61b0a1994b2d2dfa350893f31acf915c
MalachiV12303 <malachivalle12303@gmail.com>
2024-12-21 08:47:29 -0500

    added animations, may switch to react/motion but using react-fast-marquee currently

--- app/ui/animations.tsx
+++ app/ui/animations.tsx
@@ -23,7 +23,7 @@ export function PageBorder() {
         for(let a = 0; a < 40 ; a++){
             i.push((
                 <svg key={a} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
                 </svg>
         ))}
         return i;
