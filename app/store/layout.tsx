006f1655046ad97fa521b3b0d12fe21549a9f272
MalachiV12303 <malachivalle12303@gmail.com>
2025-01-25 02:17:53 -0500

    usequerystate needs to be in suspense

--- app/store/layout.tsx
+++ app/store/layout.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import IndexLink from "../ui/indexlink";
 import SearchBar from "../ui/searchbar";
+import { Spinner } from "@nextui-org/react";
 
 export default function Layout({
   children,
@@ -10,7 +12,9 @@ export default function Layout({
     <div id="storeLayoutDiv" className="relative flex">
       <div className='z-20 sm:gap-8 text-4xl fixed top-4 px-8 flex flex-col sm:flex-row sm:items-center w-full'>
         <IndexLink/>
-        <SearchBar className="text-base flex-1 sm:max-w-[40dvw] lg:max-w-[45dvw] xl:max-w-[55dvw] 2xl:max-w-[60dvw] "/>
+        <Suspense fallback={<Spinner/>}>
+          <SearchBar className="text-base flex-1 sm:max-w-[40dvw] lg:max-w-[45dvw] xl:max-w-[55dvw] 2xl:max-w-[60dvw]"/>
+        </Suspense>
       </div>
       <main id="storeLayoutMain" className="w-screen overflow-x-hidden max-h-[100dvh] h-[100dvh]">{children}</main>
     </div>
