4ec2cf18d682501dfb61ce7fc20677ce7b1beebe
MalachiV12303 <malachivalle12303@gmail.com>
2025-02-04 13:07:49 -0500

    mobile store button styling

--- app/ui/storebutton.tsx
+++ app/ui/storebutton.tsx
@@ -27,12 +27,12 @@ export default function Button({ text, lenseImage }: { text: string, lenseImage?
             setFilters(null)
             setCategory(text==='lenses' ? 'len' : null)
             scrollToSection('storeContent')
-        }} className={clsx('transition hover:scale-[1.03] delay-50 duration-500 ease-in-out bg-[url(/cameraButton.jpg)] hover:grayscale-0 sm:grayscale bg-opacity-80 bg-cover bg-center bg-no-repeat border-1 border-foreground flex items-center justify-center',  {'bg-[url(/lenseButton.jpg)]' : lenseImage? true : false})}>
+        }} className={clsx('transition hover:scale-[1.03] delay-50 duration-500 ease-in-out bg-[url(/cameraButton.jpg)] hover:grayscale-0 lg:grayscale bg-opacity-80 bg-cover bg-center bg-no-repeat border-1 border-foreground flex items-center justify-center',  {'bg-[url(/lenseButton.jpg)]' : lenseImage? true : false})}>
             <MotionConfig transition={transition}>
                 <motion.div
-                    className={`${cinzel.className} flex items-center justify-center text-white text-[3rem] lg:text-[4rem] w-[50%] h-[50%]`}
+                    className={`${cinzel.className} flex items-center justify-center text-white text-[2.5rem] lg:text-[3rem] w-[50%] h-[50%]`}
                     whileHover={{
-                        scale: 1.5,
+                        scale: 1.2,
                     }}>
                     <motion.p
                         whileHover={{
