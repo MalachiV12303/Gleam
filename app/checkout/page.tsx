6e708cdbad41e4f44a9906ea94cc908e2db4df0a
MalachiV12303 <malachivalle12303@gmail.com>
2025-02-08 16:34:28 -0500

    lint

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef, useState }  from 'react'
+import React, { useEffect, useState }  from 'react'
 import { CheckoutCart } from '../ui/checkout/checkoutcart'
 import { Accordion, AccordionItem, Spinner } from '@nextui-org/react'
 
